93137e30a96718ee6f0de911fd50d905cfdec68c
eltonlazzarin <53025782+eltonlazzarin@users.noreply.github.com>
2021-04-24 22:46:00 -0300

    useridentification page

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -1,27 +1,30 @@
 import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
 import {
   SafeAreaView,
   StyleSheet,
-  View,
   Text,
+  View,
   TextInput,
   KeyboardAvoidingView,
-  TouchableWithoutFeedback,
   Platform,
+  TouchableWithoutFeedback,
   Keyboard,
+  Alert,
 } from "react-native";
-import { useNavigation } from "@react-navigation/core";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { Button } from "../components/Button";
+
 import colors from "../styles/colors";
 import fonts from "../styles/fonts";
 
 export function UserIdentification() {
+  const navigation = useNavigation();
+
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
   const [name, setName] = useState<string>();
-
-  const navigation = useNavigation();
 
   function handleInputBlur() {
     setIsFocused(false);
@@ -33,12 +36,25 @@ export function UserIdentification() {
   }
 
   function handleInputChange(value: string) {
-    setIsFilled(!!value);
     setName(value);
+    setIsFilled(!!value);
   }
 
-  function handleSubmit() {
-    navigation.navigate("Confirmation");
+  async function handleSubmit() {
+    if (!name) return Alert.alert("Me diz como chamar você 😢");
+
+    try {
+      await AsyncStorage.setItem("@plantmanager:user", name);
+      navigation.navigate("Confirmation", {
+        title: "Prontinho",
+        subtitle: `Agora vamos começar a cuidar das suas plantinhas com muito cuidado.`,
+        buttonTitle: "Começar",
+        icon: "smile",
+        nextScreen: "PlantSelect",
+      });
+    } catch {
+      Alert.alert("Não foi possível salvar o seu nome. 😢");
+    }
   }
 
   return (
@@ -51,9 +67,10 @@ export function UserIdentification() {
           <View style={styles.content}>
             <View style={styles.form}>
               <View style={styles.header}>
-                <Text style={styles.emoji}>{isFilled ? "😄" : "😀"}</Text>
+                <Text style={styles.emoji}>{isFilled ? "😄" : "😃"}</Text>
                 <Text style={styles.title}>
-                  Como podemos {"\n"} chamar você?
+                  Como podemos {"\n"}
+                  chamar você?
                 </Text>
               </View>
 
@@ -83,39 +100,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: "100%",
-    alignItems: "center",
     justifyContent: "space-around",
+    alignItems: "center",
   },
-
   content: {
     flex: 1,
     width: "100%",
   },
-
   form: {
     flex: 1,
     justifyContent: "center",
     paddingHorizontal: 54,
     alignItems: "center",
   },
-
   header: {
     alignItems: "center",
   },
-
-  title: {
-    fontSize: 24,
-    lineHeight: 32,
-    textAlign: "center",
-    color: colors.heading,
-    fontFamily: fonts.heading,
-    marginTop: 20,
-  },
-
   emoji: {
     fontSize: 44,
   },
-
   input: {
     borderBottomWidth: 1,
     borderColor: colors.gray,
@@ -126,7 +129,14 @@ const styles = StyleSheet.create({
     padding: 10,
     textAlign: "center",
   },
-
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    textAlign: "center",
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    marginTop: 20,
+  },
   footer: {
     width: "100%",
     marginTop: 40,
