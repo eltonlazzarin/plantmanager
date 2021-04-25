1c2932dce54d5b239dbc3b343fb682e5b151fe7d
eltonlazzarin <53025782+eltonlazzarin@users.noreply.github.com>
2021-04-24 22:44:26 -0300

    confirmation page

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,32 +1,51 @@
 import React from "react";
+import { useNavigation, useRoute } from "@react-navigation/core";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import { useNavigation } from "@react-navigation/native";
 
 import { Button } from "../components/Button";
-
 import colors from "../styles/colors";
 import fonts from "../styles/fonts";
 
+interface Params {
+  title: string;
+  subtitle: string;
+  buttonTitle: string;
+  icon: "smile" | "hug";
+  nextScreen: string;
+}
+
+const emojis = {
+  hug: "🤗",
+  smile: "😄",
+};
+
 export function Confirmation() {
   const navigation = useNavigation();
+  const route = useRoute();
+
+  const {
+    title,
+    subtitle,
+    buttonTitle,
+    icon,
+    nextScreen,
+  } = route.params as Params;
 
   function handleMoveOn() {
-    navigation.navigate("PlantSelect");
+    navigation.navigate(nextScreen);
   }
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
-        <Text style={styles.emoji}>😄</Text>
+        <Text style={styles.emoji}>{emojis[icon]}</Text>
 
-        <Text style={styles.title}>Prontinho</Text>
+        <Text style={styles.title}>{title}</Text>
 
-        <Text style={styles.subtitle}>
-          Agora vamos começar a cuidar das suas plantinhas com muito cuidado.
-        </Text>
+        <Text style={styles.subtitle}>{subtitle}</Text>
 
         <View style={styles.footer}>
-          <Button title="Começar" onPress={handleMoveOn} />
+          <Button title={buttonTitle} onPress={handleMoveOn} />
         </View>
       </View>
     </SafeAreaView>
@@ -39,7 +58,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-around",
   },
-
   content: {
     flex: 1,
     justifyContent: "center",
@@ -47,11 +65,6 @@ const styles = StyleSheet.create({
     width: "100%",
     padding: 30,
   },
-
-  emoji: {
-    fontSize: 78,
-  },
-
   title: {
     fontSize: 22,
     fontFamily: fonts.heading,
@@ -60,7 +73,6 @@ const styles = StyleSheet.create({
     lineHeight: 38,
     marginTop: 15,
   },
-
   subtitle: {
     fontFamily: fonts.text,
     textAlign: "center",
@@ -68,7 +80,9 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     color: colors.heading,
   },
-
+  emoji: {
+    fontSize: 78,
+  },
   footer: {
     width: "100%",
     paddingHorizontal: 50,
